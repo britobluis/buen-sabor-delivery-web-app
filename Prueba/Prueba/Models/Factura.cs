3d86f75e9d48680798f2bdfa23e0aa334439bbf4
sebastorm <sebastian.echazu@gmail.com>
2019-05-11 14:54:08 -0300

    Facturacion y devolucion

---
+++ Prueba/Prueba/Models/Factura.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Prueba.Models
+{
+    public class Factura
+    {
+        public int ID { get; set; }
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        public DateTime Fecha { get; set; }
+        public double Monto { get; set; }
+        public ICollection<Devolucion> Devolucions { get; set; }
+
+    }
+    public class Devolucion
+    {
+
+        public int ID { get; set; }
+        [Required]
+        public int FacturaID { get; set; }
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        public DateTime FechaDevolucion { get; set; }
+        public string Motivo { get; set; }
+        public Factura Factura { get; set; }
+
+    }
+
+
+}
