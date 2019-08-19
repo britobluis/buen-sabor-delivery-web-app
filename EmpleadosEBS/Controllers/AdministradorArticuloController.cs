7000715aa0e0ff9d3f7d4f04827a0d3e8a86542f
SebastianEchazu <sebastianechazu@outlook.com>
2019-08-19 10:31:58 -0300

    modificacion parcial de la administracion de articulos de administracion

---
+++ EmpleadosEBS/Controllers/AdministradorArticuloController.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using EmpleadosEBS.Data;
+using EmpleadosEBS.Models;
+
+namespace EmpleadosEBS.Controllers
+{
+    public class AdministradorArticuloController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public AdministradorArticuloController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: AdministradorArticulo
+        public async Task<IActionResult> Index()
+        {
+            var articulo = await _context.Articulo.ToListAsync();
+
+          
+
+
+            return View(articulo);
+
+            
+        }
+
+        // GET: AdministradorArticulo/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: AdministradorArticulo/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("ID,Denominacion,PrecioCompra,PrecioVenta,EsInsumo,Stock,UnidadMedida,Aprobado")] Articulo articulo)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(articulo);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(articulo);
+        }
+
+        // GET: AdministradorArticulo/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var articulo = await _context.Articulo.FindAsync(id);
+            if (articulo == null)
+            {
+                return NotFound();
+            }
+            return View(articulo);
+        }
+
+        // POST: AdministradorArticulo/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("ID,Denominacion,PrecioCompra,PrecioVenta,EsInsumo,Stock,UnidadMedida,Aprobado")] Articulo articulo)
+        {
+            if (id != articulo.ID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(articulo);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ArticuloExists(articulo.ID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(articulo);
+        }
+
+        // GET: AdministradorArticulo/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var articulo = await _context.Articulo
+                .FirstOrDefaultAsync(m => m.ID == id);
+            if (articulo == null)
+            {
+                return NotFound();
+            }
+
+            return View(articulo);
+        }
+
+        // POST: AdministradorArticulo/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var articulo = await _context.Articulo.FindAsync(id);
+            _context.Articulo.Remove(articulo);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ArticuloExists(int id)
+        {
+            return _context.Articulo.Any(e => e.ID == id);
+        }
+    }
+}
