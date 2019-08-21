d2f35f61eb9307071f47f55441652762c9589c90
SebastianEchazu <sebastianechazu@outlook.com>
2019-08-21 11:50:06 -0300

    modifique los datos iniciales para tener u articulo en cada estado

---
+++ EmpleadosEBS/Controllers/CajeroPedidoController.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using EmpleadosEBS.Data;
+using EmpleadosEBS.Models;
+
+namespace EmpleadosEBS.Controllers
+{
+    public class CajeroPedidoController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CajeroPedidoController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: CajeroPedido
+        public async Task<IActionResult> Index()
+        {
+            var applicationDbContext = _context.Pedido.Include(p => p.EstadoPedido);
+            return View(await applicationDbContext.ToListAsync());
+        }
+
+        // GET: CajeroPedido/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var pedido = await _context.Pedido
+                .Include(p => p.EstadoPedido)
+                .FirstOrDefaultAsync(m => m.ID == id);
+            if (pedido == null)
+            {
+                return NotFound();
+            }
+
+            return View(pedido);
+        }
+     
+        // GET: CajeroPedido/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var pedido = await _context.Pedido.FindAsync(id);
+            if (pedido == null)
+            {
+                return NotFound();
+            }
+            ViewData["EstadoPedidoID"] = new SelectList(_context.EstadoPedido, "ID", "Descripcion", pedido.EstadoPedidoID);
+            return View(pedido);
+        }
+
+        // POST: CajeroPedido/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("ID,NumeroPedido,EstadoPedidoID,PorDelivery,FechaHora,PrecioVenta")] Pedido pedido)
+        {
+            if (id != pedido.ID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(pedido);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!PedidoExists(pedido.ID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["EstadoPedidoID"] = new SelectList(_context.EstadoPedido, "ID", "Descripcion", pedido.EstadoPedidoID);
+            return View(pedido);
+        }
+
+        // GET: CajeroPedido/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var pedido = await _context.Pedido
+                .Include(p => p.EstadoPedido)
+                .FirstOrDefaultAsync(m => m.ID == id);
+            if (pedido == null)
+            {
+                return NotFound();
+            }
+
+            return View(pedido);
+        }
+
+        // POST: CajeroPedido/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var pedido = await _context.Pedido.FindAsync(id);
+            _context.Pedido.Remove(pedido);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool PedidoExists(int id)
+        {
+            return _context.Pedido.Any(e => e.ID == id);
+        }
+    }
+}
--- EmpleadosEBS/Data/DatosIniciales.cs
+++ EmpleadosEBS/Data/DatosIniciales.cs
@@ -22,7 +22,9 @@ namespace EmpleadosEBS.Data
                 new Plato{ Denominacion = "Sandwich de lomo", Descripcion = "Lomo Simple",
                     Imagen = "~/img/lomo.jpg", PrecioVenta = 50,Aprobado = true},
                 new Plato{ Denominacion = "Sandwich de Lechuga", Descripcion = "Lomo con Lechuga",
-                    Imagen = "~/img/lomo2.jpg", PrecioVenta = 80, Aprobado = true}
+                    Imagen = "~/img/lomo2.jpg", PrecioVenta = 80, Aprobado = true},
+                new Plato{ Denominacion = "Pizza", Descripcion = "Lomo con Lechuga",
+                    Imagen = "", PrecioVenta = 80, Aprobado = false}
             };
             foreach (Plato p in platos)
             {
@@ -41,10 +43,13 @@ namespace EmpleadosEBS.Data
                 new Articulo{Denominacion = "Lomo",PrecioCompra = 10,PrecioVenta = 20, EsInsumo = true , Stock = 20 ,UnidadMedida = "bifes",Aprobado = true},
                 new Articulo{Denominacion = "Lechuga",PrecioCompra = 10,PrecioVenta = 20, EsInsumo = true , Stock = 20,UnidadMedida = "hojas" ,Aprobado = true},
                 new Articulo{Denominacion = "Tomate",PrecioCompra = 10,PrecioVenta = 20, EsInsumo = true , Stock = 20 , UnidadMedida ="rodajas",Aprobado = true},
+                new Articulo{Denominacion = "Prepizza",PrecioCompra = 10,PrecioVenta = 20, EsInsumo = true , Stock = 20 , UnidadMedida ="rodajas",Aprobado = false},
+                new Articulo{Denominacion = "Salsa",PrecioCompra = 10,PrecioVenta = 20, EsInsumo = true , Stock = 20 , UnidadMedida ="Lts",Aprobado = false},
+                new Articulo{Denominacion = "Queso Fresco",PrecioCompra = 10,PrecioVenta = 20, EsInsumo = true , Stock = 20 , UnidadMedida ="gramos",Aprobado = false},
                 new Articulo{Denominacion = "Coca Zero",PrecioCompra = 10,PrecioVenta = 20, EsInsumo = false , Stock = 10 , UnidadMedida ="Botella",Aprobado = true},
                 new Articulo{Denominacion = "Coca comun",PrecioCompra = 10,PrecioVenta = 20, EsInsumo = false , Stock = 10 , UnidadMedida ="Botella",Aprobado = true},
                 new Articulo{Denominacion = "Cerveza",PrecioCompra = 10,PrecioVenta = 20, EsInsumo = false , Stock = 10 , UnidadMedida ="Botella",Aprobado = false},
-                new Articulo{Denominacion = "Coca comun",PrecioCompra = 10,PrecioVenta = 20, EsInsumo = false , Stock = 10 , UnidadMedida ="Botella",Aprobado = false}
+                new Articulo{Denominacion = "Coca light",PrecioCompra = 10,PrecioVenta = 20, EsInsumo = false , Stock = 10 , UnidadMedida ="Botella",Aprobado = false}
                 };
             foreach (Articulo a in articulos)
             {
@@ -91,7 +96,7 @@ namespace EmpleadosEBS.Data
                 new EstadoPedido{Denominacion = 1, Descripcion = "Solicitado"},
                 new EstadoPedido{Denominacion = 2, Descripcion = "Aceptado"},
                 new EstadoPedido{Denominacion = 3, Descripcion = "Cocinado"},
-                new EstadoPedido{Denominacion = 4, Descripcion = "Listo para enviar/entregar"},
+                new EstadoPedido{Denominacion = 4, Descripcion = "Listo"},
                 new EstadoPedido{Denominacion = 5, Descripcion = "Entregado"},
             };
             foreach (EstadoPedido p in estados)
@@ -109,14 +114,20 @@ namespace EmpleadosEBS.Data
             }
             var pedidos = new Pedido[]
                 {
-                    new Pedido{NumeroPedido = 1000,FechaHora = DateTime.Parse("19/8/2019 12:00:00 PM" ),
+                    new Pedido{NumeroPedido = 1001,FechaHora = DateTime.Parse("19/8/2019 10:00:00 AM" ),
                         EstadoPedidoID = estados.Single(p => p.Descripcion == "Solicitado").ID,PorDelivery = true ,
                         PrecioVenta = 100 },
-                    new Pedido{NumeroPedido = 1001,FechaHora = DateTime.Parse("19/8/2019 10:00:00 PM" ),
-                        EstadoPedidoID = estados.Single(p => p.Descripcion == "Solicitado").ID,PorDelivery = true,
+                    new Pedido{NumeroPedido = 1002,FechaHora = DateTime.Parse("19/8/2019 10:30:00 AM" ),
+                        EstadoPedidoID = estados.Single(p => p.Descripcion == "Aceptado").ID,PorDelivery = false,
                         PrecioVenta = 200},
-                    new Pedido{NumeroPedido = 1002, FechaHora = DateTime.Parse("19/8/2019 11:00:00 PM"),
-                        EstadoPedidoID = estados.Single(p => p.Descripcion == "Solicitado").ID,PorDelivery = false,
+                    new Pedido{NumeroPedido = 1003, FechaHora = DateTime.Parse("19/8/2019 11:00:00 AM"),
+                        EstadoPedidoID = estados.Single(p => p.Descripcion == "Cocinado").ID,PorDelivery = true,
+                        PrecioVenta = 150},
+                    new Pedido{NumeroPedido = 1004, FechaHora = DateTime.Parse("19/8/2019 11:30:00 AM"),
+                        EstadoPedidoID = estados.Single(p => p.Descripcion == "Listo").ID,PorDelivery = false,
+                        PrecioVenta = 150},
+                    new Pedido{NumeroPedido = 1005, FechaHora = DateTime.Parse("19/8/2019 12:00:00 PM"),
+                        EstadoPedidoID = estados.Single(p => p.Descripcion == "Entregado").ID,PorDelivery = true,
                         PrecioVenta = 150}
 
                 };
@@ -133,17 +144,30 @@ namespace EmpleadosEBS.Data
             }
             var detalles = new DetPedido[]
                 {
+                    //pedido 1001--------------------------------------------------------------------------
                     new DetPedido{Cantidad = 2 ,PedidoID = pedidos.Single(p => p.NumeroPedido == 1001).ID ,
                         ArticuloID = articulos.Single(a => a.Denominacion == "Coca Zero").ID },
                     new DetPedido{Cantidad = 2, PedidoID = pedidos.Single(p => p.NumeroPedido == 1001).ID ,
                         PlatoID = platos.Single(a => a.Denominacion == "Sandwich de lomo").ID },
-                    new DetPedido{Cantidad = 1 ,PedidoID = pedidos.Single(p => p.NumeroPedido == 1000).ID ,
+                    //pedido 1002--------------------------------------------------------------------------
+                    new DetPedido{Cantidad = 1 ,PedidoID = pedidos.Single(p => p.NumeroPedido == 1002).ID ,
                         ArticuloID = articulos.Single(a => a.Denominacion == "Coca Zero").ID },
-                    new DetPedido{Cantidad = 1, PedidoID = pedidos.Single(p => p.NumeroPedido == 1000).ID ,
+                    new DetPedido{Cantidad = 1, PedidoID = pedidos.Single(p => p.NumeroPedido == 1002).ID ,
                         PlatoID = platos.Single(a => a.Denominacion == "Sandwich de lomo").ID },
-                    new DetPedido{Cantidad = 1, PedidoID = pedidos.Single(p => p.NumeroPedido == 1000).ID ,
+                    //pedido 1003--------------------------------------------------------------------------
+                    new DetPedido{Cantidad = 2, PedidoID = pedidos.Single(p => p.NumeroPedido == 1003).ID ,
                         PlatoID = platos.Single(a => a.Denominacion == "Sandwich de Lechuga").ID },
-                    new DetPedido{Cantidad = 5, PedidoID = pedidos.Single(p => p.NumeroPedido == 1002).ID ,
+                    new DetPedido{Cantidad = 2, PedidoID = pedidos.Single(p => p.NumeroPedido == 1003).ID ,
+                        PlatoID = platos.Single(a => a.Denominacion == "Sandwich de Lechuga").ID },
+                    //pedido 1004--------------------------------------------------------------------------
+                    new DetPedido{Cantidad = 1 ,PedidoID = pedidos.Single(p => p.NumeroPedido == 1004).ID ,
+                        ArticuloID = articulos.Single(a => a.Denominacion == "Coca Zero").ID },
+                    new DetPedido{Cantidad = 1, PedidoID = pedidos.Single(p => p.NumeroPedido == 1004).ID ,
+                        PlatoID = platos.Single(a => a.Denominacion == "Sandwich de lomo").ID },
+                    //pedido 1005--------------------------------------------------------------------------
+                    new DetPedido{Cantidad = 2, PedidoID = pedidos.Single(p => p.NumeroPedido == 1005).ID ,
+                        PlatoID = platos.Single(a => a.Denominacion == "Sandwich de Lechuga").ID },
+                    new DetPedido{Cantidad = 2, PedidoID = pedidos.Single(p => p.NumeroPedido == 1005).ID ,
                         PlatoID = platos.Single(a => a.Denominacion == "Sandwich de Lechuga").ID },
 
                 };
