e04cdf3b629ff76f4182d8007c920f361393a3b2
SebastianEchazu <sebastianechazu@outlook.com>
2019-08-21 10:55:48 -0300

    agrege visualizacion de pedido en el menu de administrador

---
+++ EmpleadosEBS/Controllers/AdministradorPedidoController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using EmpleadosEBS.Data;
+using EmpleadosEBS.Models;
+
+namespace EmpleadosEBS.Controllers
+{
+    public class AdministradorPedidoController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public AdministradorPedidoController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: AdministradorPedido
+        public async Task<IActionResult> Index()
+        {
+            var applicationDbContext = _context.Pedido.Include(p => p.EstadoPedido);
+            return View(await applicationDbContext.ToListAsync());
+        }
+ 
+        private bool PedidoExists(int id)
+        {
+            return _context.Pedido.Any(e => e.ID == id);
+        }
+    }
+}
