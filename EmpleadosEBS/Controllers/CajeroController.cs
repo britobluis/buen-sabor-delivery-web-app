e680f3333a1cbaa26d5e948a00f4fa707dc8623f
SebastianEchazu <sebastianechazu@outlook.com>
2019-08-22 14:43:34 -0300

    Incorpore a PedidoController de cajero CajeroPedidoController y elimine  PedidoController por no tener utilidad

--- EmpleadosEBS/Controllers/CajeroController.cs
+++ EmpleadosEBS/Controllers/CajeroController.cs
@@ -4,7 +4,9 @@ using System.Linq;
 using System.Threading.Tasks;
 using EmpleadosEBS.Data;
 using EmpleadosEBS.Models;
+using EmpleadosEBS.Models.PedidoIndexData;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 
 namespace EmpleadosEBS.Controllers
@@ -22,6 +24,10 @@ namespace EmpleadosEBS.Controllers
         {
             return View();
         }
+        /// <summary>
+        /// SECCION DE BEBIDAS
+        /// </summary>
+        /// <returns></returns>
         //-------------------------------------------------------------------------------
         // GET: Cajero/Bebida
         public async Task<IActionResult> IndexBebida()
@@ -104,6 +110,84 @@ namespace EmpleadosEBS.Controllers
         private bool ArticuloExists(int id)
         {
             return _context.Articulo.Any(e => e.ID == id);
+        }
+        /// <summary>
+        /// SECCION DE PEDIDOS
+        /// </summary>
+        /// <returns></returns>
+        //--------------------------------------------------------------------------------
+        // GET: CajeroPedido
+        public async Task<IActionResult> IndexPedido()
+        {
+            var viewModel = new PedidoIndexData();
+            viewModel.Pedidos = await _context.Pedido
+                .Include(p => p.DetPedidos)
+                    .ThenInclude(p => p.Plato)
+                .Include(d => d.EstadoPedido)
+                    .AsNoTracking()
+                    .OrderBy(i => i.FechaHora)
+                    .ToListAsync();
+
+            return View(viewModel);
+        }
+        //--------------------------------------------------------------------------------
+        // GET: Cajero/EditPedido
+        public async Task<IActionResult> EditPedido(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var pedido = await _context.Pedido.FindAsync(id);
+            if (pedido == null)
+            {
+                return NotFound();
+            }
+            ViewData["EstadoPedidoID"] = new SelectList(_context.EstadoPedido, "ID",
+                "Descripcion", pedido.EstadoPedidoID);
+            return View(pedido);
+        }
+        //--------------------------------------------------------------------------------
+        //POST: Cajero/EditPedido
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> EditPedido(int id, [Bind("ID,NumeroPedido" +
+            ",EstadoPedidoID,PorDelivery,FechaHora,PrecioVenta")] Pedido pedido)
+        {
+            if (id != pedido.ID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(pedido);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!PedidoExists(pedido.ID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(IndexPedido));
+            }
+            ViewData["EstadoPedidoID"] = new SelectList(_context.EstadoPedido, "ID"
+                , "Descripcion", pedido.EstadoPedidoID);
+            return View(pedido);
+        }
+        //-------------------------------------------------------------------------------
+        private bool PedidoExists(int id)
+        {
+            return _context.Pedido.Any(e => e.ID == id);
         }
 
     }
