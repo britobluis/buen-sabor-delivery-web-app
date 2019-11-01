8a1dc3de99ec0eca725d90e531a242a94bcb166a
Sebastian Echazu <sebastianechazu@outlook.com>
2019-11-01 19:21:30 -0300

    agrege informe de ingresos por fecha y de pedidos por fecha

--- EmpleadosEBS/Hubs/InformeHub.cs
+++ EmpleadosEBS/Hubs/InformeHub.cs
@@ -15,24 +15,36 @@ namespace EmpleadosEBS.Hubs
         {
             _context = context;
         }
-
-        public async Task InformeVentas(DateTime inicio,DateTime final) {
+        //-------------------------------------------------------------------------
+        
+        public async Task EnviarInformeIngresos(DateTime inicio,DateTime final) {
 
             double resultado = await GetIngresoAsync(inicio, final);
-
-            await Clients.All.SendAsync("informeRecibido",resultado);
+            await Clients.All.SendAsync("RecibirInformeIngresos", resultado);
         }
 
         private Task<double> GetIngresoAsync(DateTime fechaInicio, DateTime fechaFinal)
         {
-
             double ingreso;
-
             var ingresos = _context.Pedido.Where(d => d.FechaHora > fechaInicio && d.FechaHora < fechaFinal)
                                             .Select(p => p.PrecioVenta).ToList();
             ingreso = ingresos.Sum();
-
             return Task.FromResult(ingreso);
+        }
+        //-------------------------------------------------------------------------
+        public async Task EnviarInformePedidosTiempo(DateTime inicio, DateTime final)
+        {
+            double resultado = await GetPedidosAsync(inicio, final);
+            await Clients.All.SendAsync("RecibirInformePedidosTiempo", resultado);
+        }
+
+        private Task<double> GetPedidosAsync(DateTime fechaInicio, DateTime fechaFinal)
+        {
+            double pedido;
+            var pedidos = _context.Pedido.Where(d => d.FechaHora > fechaInicio && d.FechaHora < fechaFinal)
+                                            .Select(p => p.PrecioVenta).ToList();
+            pedido = pedidos.Count();
+            return Task.FromResult(pedido);
         }
     }
 }
--- EmpleadosEBS/wwwroot/js/informe.js
+++ EmpleadosEBS/wwwroot/js/informe.js
@@ -2,26 +2,52 @@
 
 var connection = new signalR.HubConnectionBuilder().withUrl("/InformeHub").build();
 
-//Disable send button until connection is established
-document.getElementById("Solicitar").disabled = true;
 
-connection.on("InformeVentas", function (resultado) {
-    resultado.value;
-    Console.log(resultado);
-    document.getElementById("Resultado").innerHTML = "informe generado";
+//Disable send button until connection is established
+document.getElementById("SolicitarIngresos").disabled  = true;
+
+//-----------------------------------------------------------------------------------
+connection.on("RecibirInformeIngresos", function (resultado) {
+    var msj = resultado.toString();
+    document.getElementById("Resultado").innerHTML = msj;
+    connection.disabled();
 });
 
 connection.start().then(function () {
-    document.getElementById("Solicitar").disabled = false;
+    document.getElementById("SolicitarIngresos").disabled = false;
 }).catch(function (err) {
     return console.error(err.toString());
 });
 
-document.getElementById("Solicitar").addEventListener("click", function (event) {
+document.getElementById("SolicitarIngresos").addEventListener("click", function (event) {
     var inicio = document.getElementById("fechaInicio").value;
     var final = document.getElementById("fechaFinal").value;
-    connection.invoke("InformeVentas", inicio, final).catch(function (err) {
+     connection.invoke("EnviarInformeIngresos", inicio, final).catch(function (err) {
         return console.error(err.toString());
     });
     event.preventDefault();
+});
+
+//-----------------------------------------------------------------------------------
+
+
+connection.on("RecibirInformePedidosTiempo", function (resultado) {
+    var msj1 = resultado.toString();
+    document.getElementById("Resultado1").innerHTML = msj1;
+});
+
+connection.start().then(function () {
+    document.getElementById("SolicitarPedidos").disabled = false;
+}).catch(function (err) {
+    return console.error(err.toString());
+});
+
+document.getElementById("SolicitarPedidos").addEventListener("click", function (event) {
+    var inicio1 = document.getElementById("fechaInicio1").value;
+    var final1 = document.getElementById("fechaFinal1").value;
+    connection.invoke("EnviarInformePedidosTiempo", inicio1, final1).catch(function (err) {
+        return console.error(err.toString());
+    });
+    event.preventDefault();
+
 });
