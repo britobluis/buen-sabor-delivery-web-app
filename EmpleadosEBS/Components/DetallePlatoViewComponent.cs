cfd06502560c1b63c028f38d1e4c5a1b747a0feb
Sebastian Echazú <sebastianechazu@outlook.com>
2019-12-07 17:33:02 -0300

    comienzo de cambios finales y prueba de componentes

---
+++ EmpleadosEBS/Components/DetallePlatoViewComponent.cs
@@ -0,0 +1,30 @@
+﻿using EmpleadosEBS.Data;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmpleadosEBS.Components
+{
+    public class DetallePlatoViewComponent : ViewComponent
+    {
+        private readonly ApplicationDbContext _context;
+
+        public DetallePlatoViewComponent(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync(int articuloId, string articuloDenominacion,)
+        {
+            var items = await GetItemsAsync(maxPriority, isDone);
+            return View(items);
+        }
+        private Task<List<TodoItem>> GetItemsAsync(int maxPriority, bool isDone)
+        {
+            return db.ToDo.Where(x => x.IsDone == isDone &&
+                                 x.Priority <= maxPriority).ToListAsync();
+        }
+    }
+}
---
+++ EmpleadosEBS/Models/PlatoDetalle.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmpleadosEBS.Models
+{
+    public class PlatoDetalle
+    {
+        public Plato Plato { get; set; }
+        public IEnumerable<DetPedido> DetPedidos { get; set; }
+        public IEnumerable<Articulo> Articulos { get; set; }
+        
+    }
+}
